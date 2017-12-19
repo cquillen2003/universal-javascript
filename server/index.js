a233b7209106903b136bbc2b492f41df1111757c
Corey Quillen <corey.quillen@gmail.com>
2017-12-19 11:29:48 -0600

    serve static assets

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 var Koa = require('koa');
 var app = new Koa();
 
+var serve = require('koa-static');
+
+var createDocument = require('./index.html');
+
+app.use(serve(__dirname + '/../public'));
+
 app.use(async ctx => {
-	ctx.body = 'Hello World';
+	ctx.body = createDocument({});
 });
 
 app.listen(3000);
