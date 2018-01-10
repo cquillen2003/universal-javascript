3d9193345d6770125c0e2b9125231d6e3cc3c680
Corey Quillen <corey.quillen@gmail.com>
2018-01-10 17:46:15 -0600

    add ssr to mobile

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import 'ignore-styles';
 import AppDesktop from '../views/apps/desktop/component';
+import AppMobile from '../views/apps/mobile/component';
 import createDocument from '../views/layouts/index.html';
 import authorize from '../middleware/authorize';
 import rootReducer from '../../src/reducers/app';
@@ -23,33 +24,64 @@ router.get('/app/*', async (ctx) => {
 	var dir = process.env.NODE_ENV === 'production' ? '../../dist' : '../../tmp';
 	var manifest = require(dir + '/manifest.json');
 
-	var store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-
 	var couchURL = 'https://' + process.env.COUCH_DB_HOST + '/' + 'universal'; //TODO: Use user's db name
-
 	couch.create(couchURL);
 
-	var context, body, props;
+	var store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+	var body, context, promises, props;
 
 	if (ctx.userAgent.isMobile) {
 
-		body = '<p>Loading...</p>';
+		//Client-side rendering
+		//body = '<p>Loading...</p>';
+
+		//Server-side rendering
+		context = { fetchers: [] };
+
+		ReactDOMServer.renderToStaticMarkup(
+			<Provider store={store}>
+				<StaticRouter basename="/app" location={ctx.url} context={context}>
+					<AppMobile/>
+				</StaticRouter>
+			</Provider>
+		);
+
+		promises = [];
+		context.fetchers.forEach(fetcher => {
+			promises.push(fetcher(store.dispatch));
+		});
+
+		await Promise.all(promises);
+
+		context = { fetchers: [] };
+		
+		body = ReactDOMServer.renderToString(
+			<Provider store={store}>
+				<StaticRouter basename="/app" location={ctx.url} context={context}>
+					<AppMobile/>
+				</StaticRouter>
+			</Provider>
+		);
 
 		props = {
 			title: 'Universal JS Mobile',
 			body: body,
 			styles: manifest['mobile.css'],
+			state: store.getState(),
+			host: process.env.COUCH_DB_HOST,
+			db: 'universal',
 			scripts: manifest['mobile.js']
 		};
 	}
 	else {
 
-		//body = '<p>Loading...</p>';
+		//Client-side rendering
+		//body = '<p>Loading...</p>'; 
 
+		//Server-side rendering
 		context = { fetchers: [] };
 
-		//Initial render to get fetchData methods via context
-		var start = Date.now();
 		ReactDOMServer.renderToStaticMarkup(
 			<Provider store={store}>
 				<StaticRouter basename="/app" location={ctx.url} context={context}>
@@ -57,10 +89,8 @@ router.get('/app/*', async (ctx) => {
 				</StaticRouter>
 			</Provider>
 		);
-		var time = Date.now() - start;
-		console.log(time + ' ms to render!');
 
-		var promises = [];
+		promises = [];
 		context.fetchers.forEach(fetcher => {
 			promises.push(fetcher(store.dispatch));
 		});
