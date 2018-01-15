7c4e9aee6d700c4cef53a22a01a903adb28bba0e
Corey Quillen <corey.quillen@gmail.com>
2018-01-15 15:07:04 -0600

    create card list drop target

---
+++ src/components/desktop/dispatch/list/component.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { DropTarget } from 'react-dnd';
+import update from 'immutability-helper';
+import { saveDoc } from '../../../../actions';
+import Card from '../card/component';
+
+class DispatchList extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			todos: this.props.todos.filter(todo => !todo.resource || !todo.hour)
+		};
+
+		this.moveCard = this.moveCard.bind(this);
+		this.findCard = this.findCard.bind(this);
+		this.addCard = this.addCard.bind(this);
+		this.schedule = this.schedule.bind(this);
+	}
+
+	/*
+	moveCard(dragIndex, hoverIndex) {
+		var newTodos = [...this.state.todos];
+
+		//Remove dragged item
+		var item = newTodos.splice(dragIndex, 1)[0];
+		console.log('Item to move', item);
+
+		//Add dragged item to new position
+		newTodos.splice(hoverIndex, 0, item);
+		console.log('newTodos', newTodos);
+
+		this.setState({ todos: newTodos });
+	}
+	*/
+
+	moveCard(dragIndex, hoverIndex) {
+		console.log('moveCard()...');
+		const { todos } = this.state
+		const dragCard = todos[dragIndex]
+
+		this.setState(
+			update(this.state, {
+				todos: {
+					$splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]]
+				},
+			})
+		)
+	}
+
+	findCard(id) {
+		var todos = this.state.todos;
+		var card = todos.find(todo => todo._id === id);
+
+		return todos.indexOf(card);
+	}
+
+	addCard(id, index) {
+		var todo = this.props.todos.find(todo => todo._id === id);
+
+		var newTodos = [...this.state.todos];
+		newTodos.splice(index, 0, todo);
+
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				todos: newTodos
+			}
+		});
+	}
+
+	schedule(todo) {
+		this.props.dispatch(saveDoc('todo', todo));
+	}
+
+	render() {
+		return this.props.connectDropTarget(
+			<div ref={(el) => { this.el = el; }} style={{ height: '500px', backgroundColor: 'gray' }}>
+				{ this.state.todos.map((todo, i) => (
+					<Card 
+						key={todo._id}
+						todo={todo}
+						findCard={this.findCard} 
+						moveCard={this.moveCard}
+						addCard={this.addCard}
+						schedule={this.schedule}
+						target
+					/>
+				)) }
+			</div>
+		)
+	}
+
+}
+
+var spec = {
+	hover: function(props, monitor, component) {
+		console.log('DispatchList.hover()....');
+		console.log(component.el.children);
+
+		var mouse = monitor.getClientOffset();
+
+		for (var i = 0; i < component.el.children.length; i++) {
+			console.log(component.el.children[i].getBoundingClientRect());
+			var card = component.el.children[i].getBoundingClientRect();
+			if (card.left < mouse.x && mouse.x < card.right) {
+				if (card.top < mouse.y && mouse.y < card.bottom) {
+					console.log('Over card index', i);
+				}
+			}
+		}
+	}
+};
+
+function collect(connect, monitor) {
+	return {
+		connectDropTarget: connect.dropTarget()
+	}
+}
+
+export default DropTarget('card', spec, collect)(DispatchList);
--- src/components/desktop/dispatch/main/component-2.js
+++ src/components/desktop/dispatch/main/component-2.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import Card from '../card/component';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
-import update from 'immutability-helper';
-import { saveDoc } from '../../../../actions';
 import CustomDragLayer from '../drag-layer/component';
+import DispatchList from '../list/component';
 import CalendarContainer from '../calendar/container';
 
 class Dispatch extends Component {
@@ -12,80 +10,13 @@ class Dispatch extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			todos: this.props.todos.filter(todo => !todo.resource || !todo.hour),
-			ref: null
-		};
+		this.state = { ref: null };
 
-		this.moveCard = this.moveCard.bind(this);
-		this.findCard = this.findCard.bind(this);
-		this.addCard = this.addCard.bind(this);
 		this.setOrigin = this.setOrigin.bind(this);
-		this.schedule = this.schedule.bind(this);
-	}
-
-	/*
-	moveCard(dragIndex, hoverIndex) {
-		var newTodos = [...this.state.todos];
-
-		//Remove dragged item
-		var item = newTodos.splice(dragIndex, 1)[0];
-		console.log('Item to move', item);
-
-		//Add dragged item to new position
-		newTodos.splice(hoverIndex, 0, item);
-		console.log('newTodos', newTodos);
-
-		this.setState({ todos: newTodos });
-	}
-	*/
-
-	moveCard(dragIndex, hoverIndex) {
-		console.log('moveCard()...');
-		const { todos } = this.state
-		const dragCard = todos[dragIndex]
-
-		this.setState(
-			update(this.state, {
-				todos: {
-					$splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]]
-				},
-			})
-		)
-	}
-
-	findCard(id) {
-		var todos = this.state.todos;
-		var card = todos.find(todo => todo._id === id);
-
-		return todos.indexOf(card);
-	}
-
-	addCard(id, index) {
-		var todo = this.props.todos.find(todo => todo._id === id);
-
-		var newTodos = [...this.state.todos];
-		newTodos.splice(index, 0, todo);
-
-		this.setState((prevState) => {
-			return {
-				...prevState,
-				todos: newTodos
-			}
-		});
 	}
 
 	setOrigin(ref) {
-		this.setState((prevState) => {
-			return {
-				...prevState,
-				ref: ref
-			};
-		});
-	}
-
-	schedule(todo) {
-		this.props.dispatch(saveDoc('todo', todo));
+		this.setState({ ref: ref });
 	}
 
 	render() {
@@ -96,18 +27,7 @@ class Dispatch extends Component {
 				<div className="row">
 					<div className="col-4">
 						<br/>
-						{ this.state.todos.map((todo, i) => (
-							<Card 
-								key={todo._id}
-								todo={todo}
-								findCard={this.findCard} 
-								moveCard={this.moveCard}
-								addCard={this.addCard}
-								schedule={this.schedule}
-								target
-							/>
-						)) }
-						
+						<DispatchList todos={this.props.todos}/>
 					</div>
 					<div className="col-8">
 						<br/>
