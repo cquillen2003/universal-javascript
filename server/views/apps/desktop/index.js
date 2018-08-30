dc370f4dcef09f48fd2e091f52273a1b06ee185a
Corey Quillen <corey.quillen@gmail.com>
2018-08-29 19:44:11 -0500

    add more comments

--- server/views/apps/desktop/index.js
+++ server/views/apps/desktop/index.js
@@ -10,6 +10,13 @@ var state = window.__PRELOADED_STATE__;
 delete window.__PRELOADED_STATE__;
 */
 
+/*
+I created this simplified index.js file to support adding a Layout component as
+in the service-base code base.
+This simplified index.js file does not support server-side rendering because
+AppDesktop contains the BrowserRouter and not the StaticRouter used on the server.
+*/
+
 var store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(<AppDesktop store={store}/>, document.getElementById('root'));
